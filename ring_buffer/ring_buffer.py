250d56e4fb6df9456804ec271edd665dc9240d47
meginks <meganjones.dev@gmail.com>
2019-09-20 11:32:59 -0500

    starting to code -- added node class and some extra methods to make the buffer writing easier

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,31 @@
+class Node:
+  """this is to hold the pointer for the queue in the buffer""" 
+    def __init__(self, data=None): 
+        self.data = data 
+        self.next = None 
+    def __str__(self): 
+        return str(self.data)
+
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
     self.current = 0
     self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+  def append(self, item): # we'd pass in an instance of Node -- , likely node.data here 
+    if self.isEmpty(): # AND IS NOT AT CAPACITY 
+      
 
   def get(self):
+    pass 
+
+  def isEmpty(self):
+    """returns true if buffer is empty""" 
+    if self.storage is [None]*self.capacity: 
+      return True 
+    else:
+      return False
+
+  def atCapacity(self): 
+    """returns true if buffer is at capacity""" 
     pass
